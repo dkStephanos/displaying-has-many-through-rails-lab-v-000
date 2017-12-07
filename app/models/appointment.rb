3f597024fc3a32d87c6332435bdade554fd4d05e
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-12-07 19:01:57 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+
+  def time
+    January 11, 2016 at 20:20
+    time = appointment_datetime.strftime("%B %M, %Y at %H:%M")
 end
