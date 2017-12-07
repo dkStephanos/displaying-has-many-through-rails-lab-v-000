cf6c018c08e4047cb8c46b3485d2fd6f4b0b9637
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-12-07 18:50:44 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207182536) do
+ActiveRecord::Schema.define(version: 20171207182701) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "appointment_datetime"
+    t.integer  "patient_id"
+    t.integer  "doctor_id"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
